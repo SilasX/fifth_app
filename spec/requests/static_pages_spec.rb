c4a4874b51d2ed5ab22d626877d5155156f257d6
Silas Barta <sbarta@gmail.com>
2013-08-04 01:46:04 -0700

    refactor tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,17 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+  let(:prefix) { "Sample App" }
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+      expect(page).to have_content('Home')
+    end
+
+    it "should have variable title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("#{prefix} | Home")
     end
   end
 
@@ -13,12 +20,22 @@ describe "StaticPages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+
+    it "should have variable title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("#{prefix} | Help")
+    end
   end
 
   describe "About Page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
+    end
+
+    it "should have variable title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("#{prefix} | About")
     end
   end
 end
